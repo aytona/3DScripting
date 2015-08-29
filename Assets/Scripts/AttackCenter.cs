06cd335ab578c3c1afbf95719f99019ece81cd96
Christopher Aytona <christopher.aytona@hotmail.com>
2015-08-28 22:05:54 -0400

    Putting Everything Together
    - Object transform
    - OnClick

---
+++ Assets/Scripts/AttackCenter.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackCenter : MonoBehaviour {
+
+	public GameObject player;
+
+	private float speed = 1f;
+
+	void Start()
+	{
+		speed += Random.Range(0f, 2f);
+	}
+
+	void Update()
+	{
+		MoveTowardsTarget();
+		PulseObject();
+	}
+
+	private void MoveTowardsTarget()
+	{
+		Vector3 targetPosition = player.transform.position;
+		Vector3 currentPosition = this.transform.position;
+
+		if(Vector3.Distance(currentPosition, targetPosition) > 1)
+		{
+			Vector3 directionOfTravel = targetPosition - currentPosition;
+			directionOfTravel.Normalize();
+
+			this.transform.Translate((directionOfTravel.x * speed * Time.deltaTime), (directionOfTravel.y * speed * Time.deltaTime), (directionOfTravel.z * speed * Time.deltaTime));
+		}
+		else{
+#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+#else
+			Application.Quit();
+#endif
+		}
+	}
+
+	private void PulseObject()
+	{
+		float rate = .5f;
+		float maxScale = 1.2f;
+		float minScale = .8f;
+
+		float scale = (Mathf.Sin (Time.time * (rate * 2 * Mathf.PI)) + 1f)/2f;
+
+		scale = Mathf.Lerp (minScale, maxScale, scale);
+		transform.localScale = new Vector3(scale,scale,scale);
+	}
+}
---
+++ Assets/Scripts/DestroyOnClick.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnClick : MonoBehaviour {
+
+	void OnMouseDown()
+	{
+		Destroy(this.gameObject);
+	}
+}
